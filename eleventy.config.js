2800fd1a131675aef199f90f5b80e75eb61550d6
Cleggss <archie.stubbs1@gmail.com>
2026-08-28 21:48:50 +0100

    attempted fix?? to embeds mainly

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,6 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/images");
-  eleventyConfig.addPassthroughCopy("./src/images/Index");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/depths");
