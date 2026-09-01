3386ce3466416e725f68e610196c8f1e959a85b1
Cleggss <archie.stubbs1@gmail.com>
2026-09-01 23:33:57 +0100

    troubleshooting and code optimisation...

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,9 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/images");
-  eleventyConfig.addPassthroughCopy("./src/images/index");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addPassthroughCopy("./src/depths");
-  eleventyConfig.addPassthroughCopy("./src/box");
-  eleventyConfig.addPassthroughCopy("./src/ocs");
   eleventyConfig.addPassthroughCopy("./src/tooltips");
-  eleventyConfig.addPassthroughCopy("./src/blogs");
 
   return {
     dir: {
